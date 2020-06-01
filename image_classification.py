18626e1323f330b119624d966086cda971d776c4
Yoni Cohen <yonicohen1993@gmail.com>
2020-06-02 01:02:50 +0300

    done Lab3 - image classification on cifar100 dataset

--- image_classification.py
+++ image_classification.py
@@ -18,8 +18,7 @@ import sys
 import cv2
 import os
 
-CIFAR100_FOLDER_PATH = 'cifar_100_python'
-CIFAR100_META_FILE_PATH = os.path.join(CIFAR100_FOLDER_PATH, 'meta')
+CIFAR100_META_FILE_PATH = 'cifar100_meta'
 WORD_VECTORS_FILE_PATH = 'wiki-news-300d-1M.vec'
 GRAPH_PATH = 'training_history_graph.png'
 DEFAULT_MODEL_PATH = 'image_model.h5'
@@ -35,8 +34,8 @@ def get_user_cli_args():
     """
     parser = argparse.ArgumentParser(description='Find the sentence most similar to given query')
     parser.add_argument('--task', choices=TASKS.keys(), help='/'.join(TASKS.keys()), required=True)
-    parser.add_argument('--model', help='trained model file global path', default=DEFAULT_MODEL_PATH)
     parser.add_argument('--image', help='image file global path')
+    parser.add_argument('--model', help='trained model file global path', default=DEFAULT_MODEL_PATH)
     return parser.parse_args()
 
 
@@ -52,7 +51,7 @@ def get_train_and_test_data():
 def get_index_to_word_label_mapper(dictionary_file_path):
     """
     Get index-to-word mapper of cifar100 dataset
-    :param dictionary_file_path: cifar100 meta binary file
+    :param dictionary_file_path: cifar100 cifar100_meta binary file
     :return: list of words, index i maps to word label #i
     """
     with open(dictionary_file_path, 'rb') as fp:
@@ -180,6 +179,63 @@ def save_model_to_h5(model_obj, model_file_path):
     logging.info(f'Model saved to {model_file_path}')
 
 
+def build_model():
+    """
+    Build CNN model
+    :return: keras model
+    """
+    width, height, depth = 32, 32, 3
+    chan_dim = -1  # channels_last format
+    input_shape = (width, height, depth)
+    padding = 'same'
+
+    model = Sequential()
+    # layer #1
+    model.add(Conv2D(32, (3, 3), padding=padding, input_shape=input_shape))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization(axis=chan_dim))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    # layer #2
+    model.add(Conv2D(64, (3, 3), padding=padding))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization(axis=chan_dim))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    # layer #3
+    model.add(Conv2D(128, (3, 3), padding=padding))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization(axis=chan_dim))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+
+    # layer #4
+    model.add(Dense(500))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization(axis=chan_dim))
+    model.add(Dropout(0.5))
+    # layer #5
+    model.add(Dense(450))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization(axis=chan_dim))
+    model.add(Dropout(0.5))
+    # layer #6
+    model.add(Dense(400))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization(axis=chan_dim))
+    model.add(Dropout(0.5))
+    # layer #7
+    model.add(Dense(350))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization(axis=chan_dim))
+    model.add(Dropout(0.5))
+    # final layer
+    model.add(Dense(300))
+    return model
+
+
 def training(train_data, train_labels, test_data, test_labels, model_file_path):
     """
     Training the model
@@ -190,58 +246,28 @@ def training(train_data, train_labels, test_data, test_labels, model_file_path):
     :param test_labels: numpy array of labels word vectors
     :param model_file_path: an absolute path for file who saves the model structure and weights
     """
-
-    # TODO: REMOVE!!!
-    # train_data, train_labels, test_data, test_labels = train_data[:2], train_labels[:2], test_data[:2], test_labels[:2]
-    # TODO: END
-
-    width, height, depth = 32, 32, 3
-    chan_dim = -1  # channels_last format
-    input_shape = (width, height, depth)
-    padding = 'same'
     # model definition
     logging.info('defining the model...')
-    model = Sequential()
-    # layer #1
-    model.add(Conv2D(32, (3, 3), padding=padding, input_shape=input_shape))
-    model.add(Activation('relu'))
-    model.add(BatchNormalization(axis=chan_dim))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    # model.add(Dropout(0.25))
-    # layer #2-3
-    for _ in range(2):
-        model.add(Conv2D(32, (3, 3), padding=padding))
-        model.add(Activation('relu'))
-        model.add(BatchNormalization(axis=chan_dim))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        # model.add(Dropout(0.25))
-    # final layer
-    model.add(Flatten())
-    model.add(Dense(300))
-    model.add(Activation('relu'))
-    model.add(BatchNormalization(axis=chan_dim))
-    # model.add(Dropout(0.5))
+    model = build_model()
     # model compilation
     logging.info('compiling the model...')
-    # mean_squared_error
-    # categorical_crossentropy
-    model.compile(optimizer=Adam(lr=0.01), loss='mean_squared_error', metrics=['accuracy'])
+    model.compile(optimizer=Adam(lr=0.01), loss='cosine_similarity', metrics=['accuracy'])
     # summarize model
     logging.info('summarize model...')
     model.summary()
     # model fitting
     logging.info('training...')
-    training_history = model.fit(train_data, train_labels, epochs=100, batch_size=10)
+    training_history = model.fit(train_data, train_labels, epochs=20, batch_size=100, validation_split=0.1)
     # model evaluation
     logging.info('evaluating...')
-    test_loss, test_accuracy = model.evaluate(test_data, test_labels, batch_size=50)
+    test_loss, test_accuracy = model.evaluate(test_data, test_labels, batch_size=100)
     print(f'evaluating scores: loss = {test_loss}, accuracy = {test_accuracy}')
-    logging.info('creating training graph...')
-    create_training_graph(training_history, GRAPH_PATH,
-                          test_history={'test_loss': test_loss, 'test_accuracy': test_accuracy})
     # save model to json and h5 files
     logging.info('save model...')
     save_model_to_h5(model, model_file_path)
+    logging.info('creating training graph...')
+    create_training_graph(training_history, GRAPH_PATH,
+                          test_history={'test_loss': test_loss, 'test_accuracy': test_accuracy}, with_validation=True)
 
 
 def train_task_handler(user_args):
@@ -250,23 +276,13 @@ def train_task_handler(user_args):
     :param user_args: user cli arguments
     """
     logging.info('loading dataset...')
-    if PROD:
-        train, test = get_train_and_test_data()
-        logging.info('loading vectors...')
-        word_vectors = load_fast_text_vectors(WORD_VECTORS_FILE_PATH)
-        logging.info('performing train pre processing...')
-        train_data, train_labels = pre_processing(train, word_vectors)
-        np.save('train_data.npy', train_data)
-        np.save('train_labels.npy', train_labels)
-        logging.info('performing test pre processing...')
-        test_data, test_labels = pre_processing(test, word_vectors)
-        np.save('test_data.npy', test_data)
-        np.save('test_labels.npy', test_labels)
-    else:
-        train_data = np.load('train_data.npy')
-        train_labels = np.load('train_labels.npy')
-        test_data = np.load('test_data.npy')
-        test_labels = np.load('test_labels.npy')
+    train, test = get_train_and_test_data()
+    logging.info('loading vectors...')
+    word_vectors = load_fast_text_vectors(WORD_VECTORS_FILE_PATH)
+    logging.info('performing train pre processing...')
+    train_data, train_labels = pre_processing(train, word_vectors)
+    logging.info('performing test pre processing...')
+    test_data, test_labels = pre_processing(test, word_vectors)
     logging.info('start training..')
     training(train_data, train_labels, test_data, test_labels, user_args.model)
     logging.info('train task done!')
@@ -288,38 +304,46 @@ def test_task_handler(user_args):
     :param user_args: user cli arguments
     """
     assert user_args.image, '--image argument is missing'
+    # loading saved model
     logging.info('loading model...')
     model = load_model(user_args.model)
     model.summary()
+    # read user image to perform prediction (image classification)
     logging.info(f'reading {user_args.image}...')
     image_tensor = read_image(user_args.image)
+    # pre precessing
     image_tensor = pixel_treatment(image_tensor.reshape(1, 32, 32, 3))
-    # TODO: REMOVE!!!
-    # train, _ = get_train_and_test_data()
-    # image_tensor = pixel_treatment(np.array([train[0][0], ]))
-    # TODO: END
+    # perform prediction
     logging.info('predicting...')
     predict_vector = model.predict(image_tensor)
+    # loading fasttext vectors
     logging.info('loading vectors...')
     word_vectors = load_fast_text_vectors(WORD_VECTORS_FILE_PATH)
+    # find the most similar vectors from fasttext
     logging.info('calculating cosine similarity...')
-    # items = np.array(list(word_vectors.items()))
-    # wv is a tuple: (word, 300D vector)
-    # similarities = np.array([{wv[0]: cosine_similarity(predict_vector, wv[1].reshape(1, 300))} for wv in items])
     similarities = []
-    i = 0
     for word, vector in word_vectors.items():
-        # if i >= 10000:
-        #     break
-        similarities.append({'word': word,
-                             'val': cosine_similarity(predict_vector, vector.reshape(1, 300))})
-        i += 1
+        similarities.append({'word': word, 'val': cosine_similarity(predict_vector, vector.reshape(1, 300))})
     logging.info(f'finding top {TOP_SIMILARITIES}...')
     similarities = sorted(similarities, key=lambda x: x['val'], reverse=True)
+    # show results
     for i in range(TOP_SIMILARITIES):
         print(f'<{similarities[i]["word"]}>', end=' ')
     print('')
     logging.info('test task done!')
+
+
+def assertions():
+    """
+    script dependencies assertions
+    """
+    cifar100_url = 'https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz'
+    assert os.path.exists(CIFAR100_META_FILE_PATH), f'''cifar100 meta file is missing!
+(download from {cifar100_url}, extract the meta file, rename to {CIFAR100_META_FILE_PATH}, move it to {os.getcwd()})'''
+
+    w2v_fasttext_url = 'https://dl.fbaipublicfiles.com/fasttext/vectors-english/wiki-news-300d-1M.vec.zip'
+    assert os.path.exists(WORD_VECTORS_FILE_PATH), f'''words-to-vectors FastText file is missing!
+(download from {w2v_fasttext_url} and move it to {os.getcwd()})'''
 
 
 # mapping task to function
@@ -333,11 +357,11 @@ def main():
     """
     Entry point of the script
     """
+    assertions()
     args = get_user_cli_args()
     task_handler = TASKS[args.task]  # get the task handler function from global dictionary mapper
     task_handler(args)
 
 
 if __name__ == '__main__':
-    PROD = False
     main()
